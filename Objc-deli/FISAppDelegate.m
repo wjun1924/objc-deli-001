55c3835913b959b2035d813f371fdd379feffa8c
wjun1924 <wjun1924@gmail.com>
2015-07-17 11:35:04 -0400

    completed deli assignment

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -21,5 +21,35 @@
  *  Define your methods here!
  
  */
+- (NSString *)stringWithDeliLine:(NSMutableArray *)deliLine{
+    NSString *line = @"The line is:";
+    
+   if ([deliLine count] == 0 ){
+        return @"The line is currently empty.";
+    }
+    else {
+            for (NSInteger i = 0; i < [deliLine count]; i++) {
+                NSString *name = [NSString stringWithFormat:@"\n%lu. %@", i+1, deliLine[i]];
+                line = [line stringByAppendingString:name];
+            };
+        
+        return line;
+    }
+};
+
+- (NSMutableArray *)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine{
+    [deliLine addObject:name];
+    return deliLine;
+};
+
+- (NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine{
+
+    NSString *firstCust = deliLine[0];
+    [deliLine removeObjectAtIndex:0];
+    
+    return firstCust;
+};
+
+
 
 @end
